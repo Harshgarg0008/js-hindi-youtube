95f08aa777bf66bc88b838c1d5cc637bc8d8cc6a
Harshgarg0008 <gargharsh0008@gmail.com>
2024-11-13 08:20:03 +0000

    done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -22,5 +22,39 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber); //output = 33
- console.log(typeof (stringNumber)) //output = string
+// console.log(stringNumber); //output = 33
+//  console.log(typeof (stringNumber)) //output = string
+
+//  ************* Operations **************** //
+let value = 3
+let negValue = -value
+// console.log(negValue); // output = -3
+
+// console.log(2+2) // Addition
+// console.log(2-2) // Subtraction
+// console.log(2*2) // Multiplication
+// console.log(2**2) // ** = raise to the power
+// console.log(2/2) // Division
+// console.log(2%2) // % = reminder
+
+let str1 = "hello"
+let str2 = " harsh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // output = 12
+// console.log(1 + "2"); // output = 12
+// console.log("1" + 2 + 2); // output = 122
+// console.log(1 + 2 + "2"); // output = 32 
+
+// console.log(true); // output = true
+// console.log(+true); // output = 1
+// console.log(+""); // output = 0 
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter); // output = 101
