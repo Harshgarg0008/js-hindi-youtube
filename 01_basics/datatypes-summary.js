98408a35adefe7fbdeb94080b70e99424f3b6386
Harshgarg0008 <gargharsh0008@gmail.com>
2024-11-13 10:31:29 +0000

    done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -34,3 +34,35 @@ console.log(typeof heroes); // output = object
 console.log(typeof anotherId); // output = symbol
 console.log(typeof scoreNumber); // output = number
 console.log(typeof LoggedIn); // output = boolean
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack(Primitive), Heap(Non-Primitive)
+
+// Stack
+let myYoutubename = "harshgargdotcom"
+
+let anothername = myYoutubename
+anothername = "harshgarg"
+
+console.log(myYoutubename); // output = harshgargdotcom
+console.log(anothername); // output = harshgarg
+
+// Heap
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "harsh@google.com"
+userTwo.upi = "harsh@ybl"
+
+console.log(userOne.email); // output = harsh@google.com
+console.log(userTwo.email); // output = harsh@google.com
+
+console.log(userOne.upi); // output = harsh@ybl
+console.log(userTwo.upi); // output = harsh@ybl
+
+
